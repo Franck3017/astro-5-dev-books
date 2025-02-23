df65a88d839aeed1400dba9081d3d74e6e3986dd
Frances Sánchez <riddickblack2015@gmail.com>
2025-02-23 18:05:43 +0100

    First Commit

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content"; // importar defineCollection y z de astro:content
+// z -> zod schema, para valir schemas, tipos de datos, formas de objetos, etc.
+
+const books = defineCollection({ // definir una colección
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    img: z.string(),
+    readtime: z.number(),
+    description: z.string(),
+    buy: z.object({
+      spain: z.string().url(),
+      usa: z.string().url(),
+    }),
+  })
+})
+
+export const collections = { books } // exportar la colección de libros
