e0396c21632ad333f7df15ffaaf13a3a4af49c3a
Frances Sánchez <riddickblack2015@gmail.com>
2025-02-23 20:33:19 +0100

    Actualizar la importación de Vercel a la versión serverless y ajustar el formato del archivo de configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
+  
   vite: {
     plugins: [tailwindcss()]
   },
@@ -16,9 +16,9 @@ export default defineConfig({
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
     }
   },
-
+  
   adapter: vercel()
 });
