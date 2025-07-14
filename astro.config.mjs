2bd766fd58febafa05db428e76a822048b5ee0c7
Frances Sánchez <riddickblack2015@gmail.com>
2025-07-14 23:24:56 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   
-  adapter: vercel({}),
+  adapter: vercel(),
 
   vite: {
     plugins: [tailwindcss()]
